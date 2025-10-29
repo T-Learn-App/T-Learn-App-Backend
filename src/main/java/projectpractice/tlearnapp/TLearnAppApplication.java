3a57d78240c2c878077329edb591bae8ad184b4e
Pasha Senko <pashasenko19@gmail.com>
2025-10-29 18:09:11 +0500

    feat: created spring repository and docker-compose with postgres

---
+++ src/main/java/projectpractice/tlearnapp/TLearnAppApplication.java
@@ -0,0 +1,13 @@
+package projectpractice.tlearnapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TLearnAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TLearnAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projectpractice/tlearnapp/TLearnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package projectpractice.tlearnapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TLearnAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
