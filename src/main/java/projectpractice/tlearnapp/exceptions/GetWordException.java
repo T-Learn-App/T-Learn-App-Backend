585393d2b0bc2072ac1ee3102db6a88c07974400
Pasha Senko <pashasenko19@gmail.com>
2025-10-31 17:35:13 +0500

    created GetWordException

---
+++ src/main/java/projectpractice/tlearnapp/exceptions/GetWordException.java
@@ -0,0 +1,7 @@
+package projectpractice.tlearnapp.exceptions;
+
+public class GetWordException extends RuntimeException {
+    public GetWordException(String message) {
+        super(message);
+    }
+}
