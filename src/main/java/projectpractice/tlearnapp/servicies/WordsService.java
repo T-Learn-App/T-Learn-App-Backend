d29a17a6968d80c667395b72ec83a959ec966883
Pasha Senko <pashasenko19@gmail.com>
2025-10-31 17:34:09 +0500

    updated words service

--- src/main/java/projectpractice/tlearnapp/servicies/WordsService.java
+++ src/main/java/projectpractice/tlearnapp/servicies/WordsService.java
@@ -2,13 +2,11 @@ package projectpractice.tlearnapp.servicies;
 
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import projectpractice.tlearnapp.dto.responses.GetWordResponse;
+import projectpractice.tlearnapp.exceptions.GetWordException;
 import projectpractice.tlearnapp.repositories.WordsRepository;
 import projectpractice.tlearnapp.repositories.entities.Word;
-
-import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -17,11 +15,15 @@ public class WordsService {
     private WordsRepository wordsRepository;
 
     public GetWordResponse getWord() {
-        Word word = wordsRepository.findRandomWord();
-        return new GetWordResponse(
-                word.getWord(),
-                word.getTranscription(),
-                word.getTranslation(),
-                word.getPartOfSpeech());
+        try {
+            Word word = wordsRepository.findRandomWord();
+            return new GetWordResponse(
+                    word.getWord(),
+                    word.getTranscription(),
+                    word.getTranslation(),
+                    word.getPartOfSpeech());
+        } catch (Exception ex) {
+            throw new GetWordException(ex.getMessage());
+        }
     }
 }
