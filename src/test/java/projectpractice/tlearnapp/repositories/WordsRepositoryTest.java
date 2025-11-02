c90955eca3a0e4c00348a356b0d87162a7edba18
Pasha Senko <pashasenko19@gmail.com>
2025-11-02 20:21:34 +0500

    updates for CI

--- src/test/java/projectpractice/tlearnapp/repositories/WordsRepositoryTest.java
+++ src/test/java/projectpractice/tlearnapp/repositories/WordsRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class WordsRepositoryTest {
 
     @Container
