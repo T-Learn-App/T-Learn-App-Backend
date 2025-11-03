ef885826a504d9d2fd6947921d1abb4e1c910133
Pasha Senko <pashasenko19@gmail.com>
2025-11-03 11:36:44 +0500

    updeted the main test

--- src/test/java/projectpractice/tlearnapp/TLearnAppApplicationTests.java
+++ src/test/java/projectpractice/tlearnapp/TLearnAppApplicationTests.java
@@ -3,10 +3,17 @@ package projectpractice.tlearnapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 class TLearnAppApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13");
 
     @Test
     void contextLoads() {
