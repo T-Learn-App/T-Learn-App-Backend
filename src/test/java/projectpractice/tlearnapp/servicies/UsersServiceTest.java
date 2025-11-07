43413ceac2c5676417d74fde695e706a4cefd14b
Pasha Senko <pashasenko19@gmail.com>
2025-11-07 23:47:39 +0500

    added tests for users service and repository

---
+++ src/test/java/projectpractice/tlearnapp/servicies/UsersServiceTest.java
@@ -0,0 +1,83 @@
+package projectpractice.tlearnapp.servicies;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import projectpractice.tlearnapp.dto.UserDto;
+import projectpractice.tlearnapp.entities.User;
+import projectpractice.tlearnapp.exceptions.DataNotFoundException;
+import projectpractice.tlearnapp.exceptions.UserAlreadyExistsException;
+import projectpractice.tlearnapp.mappers.UserMapper;
+import projectpractice.tlearnapp.repositories.UsersRepository;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Test class for UsersService")
+public class UsersServiceTest {
+
+    @Mock
+    private UsersRepository usersRepository;
+    @Mock
+    private UserMapper userMapper;
+    @InjectMocks
+    private UsersService usersService;
+
+    private Optional<User> user;
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp() {
+        user = Optional.of(new User(1L, "pasha@mail.ru"));
+        userDto = new UserDto(1L, "pasha@mail.ru");
+    }
+
+    @Test
+    public void shouldGetUserSuccessfully() {
+        when(usersRepository.findById(1L)).thenReturn(user);
+        when(userMapper.toUserDto(user.get())).thenReturn(userDto);
+
+        assertThat(usersService.getUser(1L)).isEqualTo(userDto);
+    }
+
+    @Test
+    public void shouldGetUserUnsuccessfullyWhenUserNotFound() {
+        when(usersRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThatExceptionOfType(DataNotFoundException.class)
+                .isThrownBy(() -> usersService.getUser(1L));
+    }
+
+    @Test
+    public void shouldAddUserSuccessfully() {
+        String email = "pasha@mail.ru";
+        User savedUser = User.builder().id(1L).email(email).build();
+        UserDto expectedDto = new UserDto(1L, email);
+
+        when(usersRepository.existsByEmail(email)).thenReturn(false);
+        when(usersRepository.save(any(User.class))).thenReturn(savedUser);
+        when(userMapper.toUserDto(any(User.class))).thenReturn(expectedDto);
+
+        UserDto result = usersService.addUser(email);
+
+        assertThat(result).isEqualTo(expectedDto);
+    }
+
+
+    @Test
+    public void shouldAddUserUnsuccessfullyWhenUserAlreadyExists() {
+        when(usersRepository.existsByEmail(user.get().getEmail())).thenReturn(true);
+
+        assertThatExceptionOfType(UserAlreadyExistsException.class)
+                .isThrownBy(() -> usersService.addUser(user.get().getEmail()));
+    }
+}
