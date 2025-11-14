feda92af992e143558d862e9c56e37ea71ec2ea8
Pasha Senko <pashasenko19@gmail.com>
2025-11-15 00:12:13 +0500

    added migrations

---
+++ src/main/resources/db/changelog/version1/migrations/create_table_stat_queue.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS stat_queue (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    word_id BIGINT NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    error VARCHAR(255),
+    CONSTRAINT uk_unique_id_combination UNIQUE (user_id, word_id),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_word_id FOREIGN KEY (word_id) REFERENCES words(id)
+)
