8ff0de5e7c4b4c17230ff7b72ae9e5ee977ca4fc
Pasha Senko <pashasenko19@gmail.com>
2025-12-09 15:58:20 +0500

    updated auth controller description

--- src/main/java/projectpractice/tlearnapp/controllers/AuthController.java
+++ src/main/java/projectpractice/tlearnapp/controllers/AuthController.java
@@ -1,5 +1,9 @@
 package projectpractice.tlearnapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -13,17 +17,29 @@ import projectpractice.tlearnapp.servicies.AuthService;
 @RestController("auth")
 @Validated
 @RequiredArgsConstructor
+@Tag(name = "Auth Management", description = "Operations related to authentication management")
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/login")
+    @ApiResponses(value = {
+            @ApiResponse(description = "user login or sign up successfully", responseCode = "200"),
+            @ApiResponse(description = "invalid password was sent", responseCode = "401"),
+            @ApiResponse(description = "an error occurred", responseCode = "500")
+    })
+    @Operation(summary = "Logins or signs up a user", description = "Adds a user to data base and gives him refresh and access jwt token")
     public AuthResponse login(@RequestBody @Valid AuthRequest request) {
         return authService.login(request);
     }
 
     @PostMapping("/token/refresh")
+    @ApiResponses({
+            @ApiResponse(description = "user got token successfully", responseCode = "200"),
+            @ApiResponse(description = "invalid refresh token", responseCode = "401"),
+            @ApiResponse(description = "an error occurred", responseCode = "500")
+    })
     public AuthResponse refreshToken(@RequestBody @Valid AuthRequest request) {
-        return authService.generateToken(request);
+        return authService.generateAccessToken(request);
     }
 }
